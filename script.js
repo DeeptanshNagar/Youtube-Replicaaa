c84238514a4a347c433bcc9812d6659a9ea46380
Diptansh <diptanshnagar@gmail.com>
2024-10-20 02:01:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ const menuButtons = document.querySelectorAll(".menu-button");
 const screenOverlay = document.querySelector(".main-layout .screen-overlay");
 const themeButton = document.querySelector(".navbar .theme-button i");
 
-// Toggle sidebar visibility when menu buttons are clicked
 menuButtons.forEach(button => {
   button.addEventListener("click", () => {
     document.body.classList.toggle("sidebar-hidden");
   });
 });
 
-// Toggle sidebar visibility when screen overlay is clicked
 screenOverlay.addEventListener("click", () => {
   document.body.classList.toggle("sidebar-hidden");
 });
 
-// Initialize dark mode based on localStorage
 if (localStorage.getItem("darkMode") === "enabled") {
   document.body.classList.add("dark-mode");
   themeButton.classList.replace("uil-moon", "uil-sun");
@@ -22,7 +19,6 @@ if (localStorage.getItem("darkMode") === "enabled") {
   themeButton.classList.replace("uil-sun", "uil-moon");
 }
 
-// Toggle dark mode when theme button is clicked
 themeButton.addEventListener("click", () => {
   const isDarkMode = document.body.classList.toggle("dark-mode");
   localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
